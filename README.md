88d09179591ba0844653c898b2e42945ce9761ae
josueprlt <josue.perrault@etu.unilim.fr>
2026-01-20 21:57:52 +0100

    project initialisation

---
+++ README.md
@@ -0,0 +1,6 @@
+# Typing Speed Test Project
+
+## Information
+
+This project began on **January 19, 2026**.
+This is an original project proposed by **Frontent Mentor**.
---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <h1>Typing Speed Test</h1>
+        <p class="text-red-500">{{ randomParagraph }}</p>
+    </div>
+</template>
+
+<script setup>
+const { data } = await useAsyncData('count', (_nuxtApp, { signal }) => $fetch('https://api.adviceslip.com/advice', { signal }))
+const response = JSON.parse(data.value);
+const randomParagraph = response.slip.advice;
+console.log(randomParagraph);
+
+</script>
